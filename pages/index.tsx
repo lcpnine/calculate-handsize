459ff4e5a004d2e7a87bcb901cac57c90b753e54
utek <lcpnine@gmail.com>
2021-03-18 08:47:35 +0900

    [FE] feat: add button color class to big button
    - change big button text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { BigButton } from 'components/buttons'
+import { ButtonColor } from 'constants/colors'
 
 const MainPage: FC = () => {
   return (
@@ -13,8 +14,12 @@ const MainPage: FC = () => {
         <img src="https://user-images.githubusercontent.com/52521363/111510096-caa84d80-8790-11eb-8b0e-a3c039759e24.png" />
       </div>
       <div className="main__buttons">
-        <BigButton onClick={() => {}}>button1</BigButton>
-        <BigButton onClick={() => {}}>button2</BigButton>
+        <BigButton color={ButtonColor.BLUE} onClick={() => {}}>
+          I have a picture
+        </BigButton>
+        <BigButton color={ButtonColor.WHITE} onClick={() => {}}>
+          I will take a picture
+        </BigButton>
       </div>
     </div>
   )
