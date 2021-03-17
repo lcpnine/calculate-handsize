7bd58eb8d60cbea65842b0040b260010ef898e81
utek <lcpnine@gmail.com>
2021-03-18 01:50:31 +0900

    [FE] chores: apply prettier

--- utils/myAxios.ts
+++ utils/myAxios.ts
@@ -1,24 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios'
 
 const baseAxios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   withCredentials: true,
   responseType: 'json',
   timeout: 5000,
-});
+})
 
 const myAxios = {
   get: (endpoint: string): Promise<AxiosResponse> => baseAxios.get(endpoint),
 
-  post: (endpoint: string, body: any): Promise<AxiosResponse> =>
-    baseAxios.post(endpoint, body),
+  post: (endpoint: string, body: any): Promise<AxiosResponse> => baseAxios.post(endpoint, body),
 
-  put: (endpoint: string, body: any): Promise<AxiosResponse> =>
-    baseAxios.put(endpoint, body),
+  put: (endpoint: string, body: any): Promise<AxiosResponse> => baseAxios.put(endpoint, body),
 
-  delete: (endpoint: string): Promise<AxiosResponse> =>
-    baseAxios.delete(endpoint),
-};
+  delete: (endpoint: string): Promise<AxiosResponse> => baseAxios.delete(endpoint),
+}
 
-export default myAxios;
+export default myAxios
