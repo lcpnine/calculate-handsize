e49571c76fd4a4503cac032eb31dde6c469b4654
utek <lcpnine@gmail.com>
2021-03-18 02:27:55 +0900

    [FE] feat: set index page skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
-import { FC } from 'react';
+import { FC } from 'react'
+import { BigButton } from 'components/buttons'
 
 const MainPage: FC = () => {
-  return <div>MyHand</div>;
-};
+  return (
+    <div className="main">
+      <div className="main__text">
+        <div className="main__text-title">Show me the hand size</div>
+        <div className="main__text-description">This is description</div>
+      </div>
+      <div className="main__examples">
+        <img src="https://user-images.githubusercontent.com/52521363/111510104-cda33e00-8790-11eb-9fd9-f541026e6349.jpg" />
+        <img src="https://user-images.githubusercontent.com/52521363/111510096-caa84d80-8790-11eb-8b0e-a3c039759e24.png" />
+      </div>
+      <div className="main__buttons">
+        <BigButton onClick={() => {}}>button1</BigButton>
+        <BigButton onClick={() => {}}>button2</BigButton>
+      </div>
+    </div>
+  )
+}
 
-export default MainPage;
+export default MainPage
